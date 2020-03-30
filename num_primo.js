ef4b2b315569ae7e6e3464885aa4c8da1e10b43e
leonel <leonell_ceolin@hotmail.com>
2020-03-30 09:24:53 -0400

    função que verifica se um numero é primo

---
+++ num_primo.js
@@ -0,0 +1,9 @@
+function primo(num) {
+  var divisores = 0;
+
+  for (var count = 1; count <= num; count++)
+    if (numero % count == 0) divisores++;
+
+  if (divisores == 2) return "É primo";
+  else return "Não é primo";
+}
